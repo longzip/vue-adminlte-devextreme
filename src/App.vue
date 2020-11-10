c4402e56d9bbd4986d107ae759d752b88152885d
Lo Van Long <longlv@v-auto.com.vn>
2020-11-10 10:21:26 +0700

    1st

--- src/App.vue
+++ src/App.vue
@@ -7,26 +7,39 @@
     <router-view />
   </div>
 </template>
+<script>
+// Dictionaries for German and Russian languages
+import viMessages from "devextreme/localization/messages/ru.json";
+import { locale, loadMessages } from "devextreme/localization";
+// ===== Vue ======
+export default {
+  created() {
+    loadMessages(viMessages);
+    locale(navigator.language);
+  }
+};
+</script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
 
-#nav {
-  padding: 30px;
+// #nav {
+//   padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
+//
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,44 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <DxChart :data-source="data">
+    <DxArgumentAxis :tick-interval="10" />
+    <DxSeries type="bar" />
+    <DxLegend :visible="false" />
+  </DxChart>
 </template>
+
+<script>
+import DxChart, {
+  DxArgumentAxis,
+  DxSeries,
+  DxLegend
+} from "devextreme-vue/chart";
+
+const data = [
+  {
+    arg: 1990,
+    val: 5320816667
+  },
+  {
+    arg: 2000,
+    val: 6127700428
+  },
+  {
+    arg: 2010,
+    val: 6916183482
+  }
+];
+
+export default {
+  components: {
+    DxChart,
+    DxArgumentAxis,
+    DxSeries,
+    DxLegend
+  },
+  data() {
+    return {
+      data
+    };
+  }
+};
+</script>
